fc6a5a838c776af23339627b33e8f91c634c9150
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-10-10 23:03:01 +0200

    feat: fetch data from weatherbit api

---
+++ classes/Weather.js
@@ -0,0 +1,35 @@
+export default class Weather {
+    constructor() {
+      this.getLocation();
+    }
+  
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(
+        this.showPosition.bind(this), 
+        this.showError.bind(this)   
+        );
+    }
+  
+    showPosition(position) {
+      let lat = position.coords.latitude;
+      let lng = position.coords.longitude;
+      console.log(lat, lng);
+      this.getWeather(lat, lng);
+    }
+  
+    getWeather(lat, lng) {
+      let url= `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lng}&key=12d97956d95243be85758969f0a7fb99`
+        fetch(url).then(response => {
+            return response.json();
+        }).then(data => {
+            document.querySelector("p").innerHTML = document.querySelector("p").innerHTML+ " " + data.data[0].temp + " °C";
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+  
+    showError(error) {
+      console.log(error);
+    }
+  }
+  
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-import './style.css'
+import './style.css';
+import Weather from './classes/weather.js';
+
+const weather = new Weather();
